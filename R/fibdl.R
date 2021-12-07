ab43cefa9c265f95429976518eca21124825fdbc
Boutry <sebastien.boutry@inrae.fr>
2021-12-07 10:44:15 +0100

    ajout d'une fonction intégrative

---
+++ R/fibdl.R
@@ -0,0 +1,22 @@
+
+#' Fonction intégrative
+#'
+#' @param listflor
+#' @param info_uo
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fibdl <- function(listflor, info_uo) {
+  table_ibdl <- listflor %>%
+    fibdl_listflor_transcode() %>%
+    fibdl_listflor_validate() %>%
+    fibdl_listflor_metrics() %>%
+    fibdl_listflor_EQR(data=.,add_information_station=info_uo) %>%
+    fibdl_listflor_EQR_standard() %>%
+    fibdl_uo_score() %>%
+    fibdl_ibdl() %>%
+    fibdl_ibdl_qualify()
+  return(table_ibdl)
+}
