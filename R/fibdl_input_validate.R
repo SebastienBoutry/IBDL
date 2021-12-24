44e42e1b8ac3b98595dd3a382243cb4802f4bc34
Boutry <sebastien.boutry@inrae.fr>
2021-12-24 16:32:17 +0100

    fonction fibdl_checkof_listflor

--- R/fibdl_input_validate.R
+++ R/fibdl_input_validate.R
@@ -1,11 +1,37 @@
 fibdl_checkof_listflor <- function(data){
   ##
-  step_1 <- CheckOfTable::fcot_table(data,table_orig="liste flore")
-  if(step_1$){
-    step_2 <- CheckOfTable::fcot_table(data,table_orig="liste flore")
+  table_orig <- "liste flore"
+  ##
+  step_1 <- CheckOfTable::fcot_table(data,table_orig=table_orig)
+  if(step_1$valeur_test=="Oui"){
+    step_2 <- CheckOfTable::fcot_colnames(data,table_orig=table_orig)
+    if(step_2$valeur_test=="Oui"){
+      bind_rows(
+      #   fcot_value_vide(table_test,
+      #                   c("a","b","c","d"),
+      #                   table_orig),
+      #   fcot_type_character(table_test,
+      #                       c("b"),
+      #                       table_orig),
+      #   fcot_type_numeric(table_test,
+      #                     c("a","b","c","d"),
+      #                     table_orig),
+      #   fcot_value_positive(table_test,
+      #                       c("a","b","c","d"),
+      #                       table_orig),
+      #   fcot_value_interval(table_test,
+      #                       c("a","c"),
+      #                       mini=2,
+      #                       maxi=5,table_orig)
+      # ) %>%
+      #   select(message)
+    }else{
+      return(step_2)
+    }
   }else{
-
+    return(step_1)
   }
+}
   # bind_rows(
   #   f_format_table(data,table_orig="liste flore"),
   #   f_format_colnames(data,
