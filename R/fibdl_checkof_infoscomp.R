ad4d4355c43f35a2942f6fe157bf6e4db7dca004
Boutry <sebastien.boutry@inrae.fr>
2021-12-29 08:53:58 +0100

    création de la fonction fibdl_checkof_infoscomp

--- R/fibdl_checkof_infoscomp.R
+++ R/fibdl_checkof_infoscomp.R
@@ -1,6 +1,6 @@
-#' Contrôle du tableau liste floristique pour le calcul de l'IBDL
+#' Contrôle du tableau informations complémentaires sur le site pour le calcul de l'IBDL
 #'
-#' @param data tableau représentant la liste floristique
+#' @param data tableau représentant les informations complémentaires sur l'unité d'observation
 #'
 #' @return
 #' @export
@@ -8,37 +8,37 @@
 #'
 #' @examples
 #' list_flor <- read.csv2(system.file("listflor.csv", package = "IBDL"),fileEncoding = "utf-8")
-#' fibdl_checkof_listflor(list_flor)
+#' fibdl_checkof_infoscomp(list_flor)
 
-fibdl_checkof_listflor <- function(data){
+fibdl_checkof_infoscomp <- function(data){
   ##
-  table_orig <- "liste flore"
+  table_orig <- "informations complémentaires sur l'uo"
   ##
   step_1 <- CheckOfTable::fcot_table(data,table_orig=table_orig)
   sortie <- step_1
   if(step_1$valeur_test=="oui"){
     step_2 <- CheckOfTable::fcot_colnames(data,
-                                          noms_colonnes = c("id_prelevement","taxons","ab"),
+                                          noms_colonnes = c("id_prelevement", "id_uo", "nature_substrat", "id_campagne", "type_dominant", "code_pe",  "datedebut"),
                                           table_orig=table_orig)
     sortie <- dplyr::bind_rows(sortie,step_2)
     if(step_2$valeur_test=="oui"){
       step_3 <- dplyr::bind_rows(
         CheckOfTable::fcot_value_empty(data,
-                                       c("id_prelevement","taxons","ab"),
+                                       c("id_prelevement", "id_uo", "nature_substrat", "id_campagne", "type_dominant", "code_pe",  "datedebut"),
                                        table_orig),
         CheckOfTable::fcot_type_character(data,
-                                          c("taxons"),
+                                          c("nature_substrat","code_pe"),
                                           table_orig),
         CheckOfTable::fcot_type_numeric(data,
-                                        c("id_prelevement","ab"),
+                                        c("id_prelevement", "id_uo", "id_campagne", "type_dominant"),
                                         table_orig),
         CheckOfTable::fcot_value_positive(data,
-                                          c("id_prelevement","ab"),
+                                          c("id_prelevement", "id_uo", "id_campagne", "type_dominant"),
                                           table_orig),
         CheckOfTable::fcot_value_interval(data,
-                                          c("ab"),
+                                          c("type_dominant"),
                                           mini=1,
-                                          maxi=400,
+                                          maxi=4,
                                           table_orig)
       )
       step_3 <- step_3 %>%
