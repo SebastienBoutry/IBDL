12040539de7500e96169921a983f8ad6904bf295
Boutry <sebastien.boutry@inrae.fr>
2023-04-06 14:43:14 +0200

    Mise à jour majeure 06-04-2023

--- R/fibdl_listflor_qualify.R
+++ R/fibdl_listflor_qualify.R
@@ -19,37 +19,44 @@
 #'
 fibdl_listflor_qualify <- function(data,
                                    seuil_nbr_valves = 350,
-                                   seuil_pour_valves_sp = 0.25,
+                                   seuil_pour_valves_sp = 0.50,
                                    seuil_pour_taxons_indiciels = 0.75,
                                    table_tax_alerts = IBDL:::Table_taxons_alertes){
   ## create table validation
   table_validation <- data %>%
     ## récupération des taxons d'alertes
     dplyr::left_join(table_tax_alerts,
-              by = c("taxons" = "taxons_indiciels")
+                     by = c("taxons" = "taxons_indiciels")
     ) %>%
     ## validation des taxons
     dplyr::mutate(indiciel=ifelse(is.na(indiciel),0,indiciel)) %>%
     dplyr::group_by(id_prelevement) %>%
     dplyr::summarise(tot=sum(ab),
-              tot_sans_ADMI=sum(ab[taxons!="ADMI" &! rang %in% c("erreur","gn",NA_character_)]),
-              Ab_tot_indiciel = sum(ab[indiciel==1])) %>%
+                     r_esp=sum(ab[! rang %in% c("erreur","gn",NA_character_)])/tot,
+                     r_ADMI = sum(ab[!taxons %in% "ADMI"]) / tot,
+                     Ab_tot_indiciel = sum(ab[indiciel==1])) %>%
     dplyr::mutate(r_esp=tot_sans_ADMI/tot,
-           pour_taxons_indiciels=Ab_tot_indiciel/tot) %>%
+                  pour_taxons_indiciels=Ab_tot_indiciel/tot) %>%
     dplyr::mutate(nbr_valves =ifelse(tot>=seuil_nbr_valves,
                                      TRUE,
                                      FALSE)) %>%
     dplyr::mutate(r_esp = ifelse(r_esp >=seuil_pour_valves_sp ,
                                  TRUE,
                                  FALSE)) %>%
-    dplyr::mutate(r_taxons_indiciels = ifelse(pour_taxons_indiciels >= seuil_pour_taxons_indiciels,
-                                              TRUE,
-                                              FALSE)) %>%
+    # dplyr::mutate(r_taxons_indiciels = ifelse(pour_taxons_indiciels >= seuil_pour_taxons_indiciels,
+    #                                           TRUE,
+    #                                           FALSE)) %>%
     dplyr::mutate(Validation=ifelse(r_esp==TRUE &
-                                      nbr_valves ==TRUE &
-                                      r_taxons_indiciels==TRUE,
+                                      nbr_valves ==TRUE,
+                                    # r_taxons_indiciels==TRUE,
                                     TRUE,
                                     FALSE))
+  ## identifaction des prelevement ayant une liste floristique avec beaucoup trop ADMI
+  id_prelevement_admi <- table_validation %>%
+    mutate(Validation_admi = ifelse(r_ADMI >= 0.5, TRUE, FALSE)) %>%
+    filter(Validation_admi == FALSE) %>%
+    pull(id_prelevement)
+
   ##
   return(table_validation)
 }
