3f68dbde23d13bca975d9b259d247cddd80eabf4
Boutry <sebastien.boutry@inrae.fr>
2023-04-06 16:47:27 +0200

    nettoyage de campagne macrophyte

--- R/fibdl_ibdl.R
+++ R/fibdl_ibdl.R
@@ -14,7 +14,8 @@ fibdl_ibdl<- function(data){
     dplyr::group_by(code_pe,id_campagne,datedebut) %>%
     dplyr::mutate(nbr_uo=length(id_uo)) %>%
     dplyr::ungroup() %>%
-    tidyr::unite("join",c("code_pe","type_dominant"),remove=FALSE) %>%
+    tidyr::unite("join",c("code_pe","type_dominant"),
+                 remove=FALSE) %>%
     dplyr::select(-type_dominant) %>%
     ##
     dplyr::left_join(table_pourcentage_type,
@@ -23,7 +24,7 @@ fibdl_ibdl<- function(data){
     dplyr::rename("code_lac"="code_pe") %>%
     ##
     dplyr::group_by(id_campagne,datedebut,code_lac,pourcentage,value,nbr_uo) %>%
-    dplyr::summarise(Moy_type=mean(Note_UO,na.rm=TRUE)) %>%
+    dplyr::summarise(Moy_type=min(Note_UO,na.rm=TRUE)) %>%
     dplyr::ungroup() %>%
     dplyr::arrange(id_campagne) %>%
     dplyr::mutate(Note_type=Moy_type*(value/100)) %>%
@@ -35,7 +36,7 @@ fibdl_ibdl<- function(data){
               ) %>%
     dplyr::mutate(ClassIBDL=cut(IBDL,c(0,0.2,0.4,0.6,0.8,1),
                          labels=c("B","P","M","G","HG"))) %>%
-    dplyr::mutate(IBDL=IBDL/SommePourcent) %>%
+    dplyr::mutate(IBDL=round(IBDL/SommePourcent),2) %>%
     dplyr::ungroup()
   ##
   return(table_ibdl)
