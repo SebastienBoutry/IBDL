d3f7141d2261a7f7a7b4ecf66b1404eaf59c12ce
Boutry <sebastien.boutry@inrae.fr>
2023-04-11 10:32:46 +0200

    suppression des arrondues dans les formules

--- R/fibdl_listflor_EQR_standard.R
+++ R/fibdl_listflor_EQR_standard.R
@@ -12,14 +12,14 @@
 
 fibdl_listflor_EQR_standard <- function(data){
   table_listflor_EQR_standard <- data %>%
-    dplyr::mutate(EQR=dplyr::case_when(EQR>=0.968 & metrics=="DBO5"~EQR*6.35-5.35,
-                         EQR<0.968 & metrics=="DBO5"~EQR*0.826,
-                         EQR>=0.972 & metrics=="MES"~EQR*7.14-6.14,
-                         EQR<0.972 & metrics=="MES"~EQR*0.823,
-                         EQR>=0.981 & metrics=="NKJ"~EQR*10.7-9.70,
-                         EQR<0.981 & metrics=="NKJ"~EQR*0.815,
-                         EQR>=0.955 & metrics=="Ptot"~EQR*4.45-3.45,
-                         EQR<0.955 & metrics=="Ptot"~EQR*0.838)) %>%
+    dplyr::mutate(EQR=dplyr::case_when(EQR>=0.968 & metrics=="DBO5"~EQR*6.348627-5.348627,
+                         EQR<0.968 & metrics=="DBO5"~EQR*0.8260221 ,
+                         EQR>=0.972 & metrics=="MES"~EQR*7.139655-6.139655,
+                         EQR<0.972 & metrics=="MES"~EQR*0.8230559 ,
+                         EQR>=0.981 & metrics=="NKJ"~EQR*10.701637 -9.701637,
+                         EQR<0.981 & metrics=="NKJ"~EQR*0.8152357,
+                         EQR>=0.955 & metrics=="Ptot"~EQR*4.453086 -3.453086 ,
+                         EQR<0.955 & metrics=="Ptot"~EQR* 0.8376197 )) %>%
     dplyr::mutate(EQR=ifelse(EQR>1,1,EQR)) %>%
     dplyr::select(id_prelevement,metrics,id_uo,nature_substrat,type_dominant,id_campagne,code_pe,datedebut,classi_alc,EQR)
   return(table_listflor_EQR_standard)
