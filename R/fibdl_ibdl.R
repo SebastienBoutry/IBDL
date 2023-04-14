65338ecacda430a01f18d27593b41b6772f23d11
Boutry <sebastien.boutry@inrae.fr>
2023-04-14 11:13:51 +0200

    ajout dans le group_by nbr_uo_type

--- R/fibdl_ibdl.R
+++ R/fibdl_ibdl.R
@@ -27,7 +27,7 @@ fibdl_ibdl<- function(data){
     mutate(nbr_uo_type = n()) %>%
     ungroup() %>%
     ##
-    dplyr::group_by(id_campagne,datedebut,code_lac,pourcentage,value,nbr_uo,commentaires_sep) %>%
+    dplyr::group_by(id_campagne,datedebut,code_lac,pourcentage,value,nbr_uo,commentaires_sep,nbr_uo_type) %>%
     dplyr::summarise(Min_type=min(Note_UO,na.rm=TRUE)) %>%
     filter(!(str_detect(commentaires_sep, "pas fiable") & nbr_uo_type >= 1)) %>%
     dplyr::ungroup() %>%
