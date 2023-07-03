56c059b8fb523a6ed4f8f5c15297d11465be8870
Boutry <sebastien.boutry@inrae.fr>
2023-07-03 14:22:41 +0200

    sans IBDL pour la table_transcodage

--- R/fibdl_listflor_transcode.R
+++ R/fibdl_listflor_transcode.R
@@ -16,7 +16,7 @@ fibdl_listflor_transcode <- function(data){
 
 
   table_listflor_transcode <- data %>%
-    dplyr::left_join(IBDL::table_transcodage %>%
+    dplyr::left_join(table_transcodage %>%
                        dplyr::select(abre, code_espece_boucle2,
                                      denominations_sans_auteur.y,niveau2),
                      by = c("taxons" = "abre")
